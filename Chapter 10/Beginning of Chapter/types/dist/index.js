a069d1ef0e26d93f823885f4a1d8d6415a94add0
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-19 16:59:35 +0100

    11.3 Using classes

--- Chapter 10/Beginning of Chapter/types/dist/index.js
+++ Chapter 10/Beginning of Chapter/types/dist/index.js
@@ -1,12 +1,18 @@
-let Employee = function (id, name, dept, city) {
-    this.id = id;
-    this.name = name;
-    this.dept = dept;
-    this.city = city;
-};
-Employee.prototype.writeDept = function () {
-    console.log(`${this.name} works in ${this.dept}`);
-};
+class Employee {
+    id;
+    name;
+    dept;
+    city;
+    constructor(id, name, dept, city) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.city = city;
+    }
+    writeDept() {
+        console.log(`${this.name} works in ${this.dept}`);
+    }
+}
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
 let data = [
     { id: "bsmith", name: "Bob Smith", city: "Lodon" },
--- Chapter 10/Beginning of Chapter/types/src/index.ts
+++ Chapter 10/Beginning of Chapter/types/src/index.ts
@@ -4,24 +4,23 @@ type Person = {
   city: string,
 };
 
-type Employee = {
-  id: string,
-  name: string,
-  dept: string,
-  city: string,
-  writeDept: () => void
+class Employee {
+  id: string;
+  name: string;
+  dept: string;
+  city: string;
+
+  constructor(id: string, name: string, dept: string, city: string) {
+    this.id = id;
+    this.name = name;
+    this.dept = dept;
+    this.city = city;
+  }
+
+  writeDept() {
+    console.log(`${this.name} works in ${this.dept}`);
+  }
 }
-
-let Employee = function (id: string, name: string, dept: string, city: string) {
-  this.id = id;
-  this.name = name;
-  this.dept = dept;
-  this.city = city;
-};
-
-Employee.prototype.writeDept = function () {
-  console.log(`${this.name} works in ${this.dept}`);
-};
 
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
 
@@ -33,7 +32,7 @@ let data: (Person | Employee)[] = [
 ];
 
 data.forEach((item) => {
-  if ("dept" in item) {
+  if (item instanceof Employee) {
     item.writeDept();
   } else {
     console.log(`${item.id} ${item.name}, ${item.city}`);
