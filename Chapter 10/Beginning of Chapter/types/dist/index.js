44dfd1a72d65a0651feb03eaefe183aba04d00b7
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-19 16:40:08 +0100

    11.2 Using constructor functions

--- Chapter 10/Beginning of Chapter/types/dist/index.js
+++ Chapter 10/Beginning of Chapter/types/dist/index.js
@@ -1,13 +1,24 @@
-function calculatePrice(quantity, price) {
-    return quantity * price;
-}
-let total = calculatePrice(2, 19.99);
-console.log(`Price: ${total}`);
-function getRandomValue() {
-    return Math.floor(Math.random() * 4) + 1;
-}
-function getCityString(city) {
-    return `City: ${city}`;
-}
-let str = getCityString("London");
-console.log(str);
+let Employee = function (id, name, dept, city) {
+    this.id = id;
+    this.name = name;
+    this.dept = dept;
+    this.city = city;
+};
+Employee.prototype.writeDept = function () {
+    console.log(`${this.name} works in ${this.dept}`);
+};
+let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
+let data = [
+    { id: "bsmith", name: "Bob Smith", city: "Lodon" },
+    { id: "ajones", name: "Alice Jones", city: "Paris" },
+    { id: "dpeters", name: "Dora Peters", city: "New York" },
+    salesEmployee,
+];
+data.forEach((item) => {
+    if (item instanceof Employee) {
+        item.writeDept();
+    }
+    else {
+        console.log(`${item.id} ${item.name}, ${item.city}`);
+    }
+});
--- Chapter 10/Beginning of Chapter/types/src/index.ts
+++ Chapter 10/Beginning of Chapter/types/src/index.ts
@@ -4,12 +4,29 @@ type Person = {
   city: string;
 };
 
-let data: Person[] = [
+let Employee = function (id: string, name: string, dept: string, city: string) {
+  this.id = id;
+  this.name = name;
+  this.dept = dept;
+  this.city = city;
+};
+Employee.prototype.writeDept = function () {
+  console.log(`${this.name} works in ${this.dept}`);
+};
+
+let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
+
+let data: (Person | Employee)[] = [
   { id: "bsmith", name: "Bob Smith", city: "Lodon" },
   { id: "ajones", name: "Alice Jones", city: "Paris" },
   { id: "dpeters", name: "Dora Peters", city: "New York" },
+  salesEmployee,
 ];
 
 data.forEach((item) => {
-  console.log(`${item.id} ${item.name}, ${item.city}`);
+  if (item instanceof Employee) {
+    item.writeDept();
+  } else {
+    console.log(`${item.id} ${item.name}, ${item.city}`);
+  }
 });
