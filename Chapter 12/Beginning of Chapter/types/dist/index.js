621f408a0d96d7c6169328708417704d5cfd28a8
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-29 18:56:47 +0100

    12.3.4 Defining multiple type parameters

--- Chapter 12/Beginning of Chapter/types/dist/index.js
+++ Chapter 12/Beginning of Chapter/types/dist/index.js
@@ -4,29 +4,23 @@ let people = [
     new Person("Dora Peters", "New York"),
 ];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-let cities = [new City("Londong", 8136000), new City("Paris", 2141000)];
+let cities = [new City("London", 8136000), new City("Paris", 2141000)];
 class DataCollection {
     items = [];
     constructor(intialItems) {
         this.items.push(...intialItems);
     }
-    add(newItem) {
-        this.items.push(newItem);
-    }
-    getNames() {
-        return this.items.map((item) => item.name);
-    }
-    getItem(index) {
-        return this.items[index];
+    collate(targetData, itemProp, targetProp) {
+        let results = [];
+        this.items.forEach((item) => {
+            let match = targetData.find((d) => d[targetProp] === item[itemProp]);
+            if (match !== undefined) {
+                results.push({ ...match, ...item });
+            }
+        });
+        return results;
     }
 }
 let peopleData = new DataCollection(people);
-let firstPerson = peopleData.getItem(0);
-console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-console.log(`Person Names: ${peopleData.getNames().join(", ")}`);
-let productData = new DataCollection(products);
-let firstProduct = productData.getItem(0);
-console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
-console.log(`Product Names: ${productData.getNames().join(", ")}`);
-let cityData = new DataCollection(cities);
-console.log(`City Names: ${cityData.getNames().join(", ")}`);
+let collatedData = peopleData.collate(cities, "city", "name");
+collatedData.forEach((c) => console.log(`${c.name}, ${c.city}, ${c.population}`));
--- Chapter 12/Beginning of Chapter/types/src/index.ts
+++ Chapter 12/Beginning of Chapter/types/src/index.ts
@@ -5,37 +5,29 @@ let people = [
   new Person("Dora Peters", "New York"),
 ];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-let cities = [new City("Londong", 8136000), new City("Paris", 2141000)];
+let cities = [new City("London", 8136000), new City("Paris", 2141000)];
 
-class DataCollection<T extends { name: string }> {
+class DataCollection<T extends { name: string }, U> {
   private items: T[] = [];
 
   constructor(intialItems: T[]) {
     this.items.push(...intialItems);
   }
 
-  add(newItem: T) {
-    this.items.push(newItem);
-  }
-
-  getNames(): string[] {
-    return this.items.map((item) => item.name);
-  }
-
-  getItem(index: number): T {
-    return this.items[index];
+  collate(targetData: U[], itemProp: string, targetProp: string): (T & U)[] {
+    let results = [];
+    this.items.forEach((item) => {
+      let match = targetData.find((d) => d[targetProp] === item[itemProp]);
+      if (match !== undefined) {
+        results.push({ ...match, ...item });
+      }
+    });
+    return results;
   }
 }
 
-let peopleData = new DataCollection<Person>(people);
-let firstPerson = peopleData.getItem(0);
-console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-console.log(`Person Names: ${peopleData.getNames().join(", ")}`);
-
-let productData = new DataCollection<Product>(products);
-let firstProduct = productData.getItem(0);
-console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
-console.log(`Product Names: ${productData.getNames().join(", ")}`);
-
-let cityData = new DataCollection<City>(cities);
-console.log(`City Names: ${cityData.getNames().join(", ")}`);
+let peopleData = new DataCollection<Person, City>(people);
+let collatedData = peopleData.collate(cities, "city", "name");
+collatedData.forEach((c) =>
+  console.log(`${c.name}, ${c.city}, ${c.population}`)
+);
