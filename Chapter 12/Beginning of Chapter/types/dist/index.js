ca682375300d5c93c87a2d03a826e781e3b0cadf
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-11-29 17:34:41 +0100

    12.2 Understanding the problem solved by generic types

--- Chapter 12/Beginning of Chapter/types/dist/index.js
+++ Chapter 12/Beginning of Chapter/types/dist/index.js
@@ -1,20 +1,25 @@
-class SportsProduct {
-    name;
-    category;
-    price;
-    constructor(name, category, price) {
-        this.name = name;
-        this.category = category;
-        this.price = price;
-        // no statements required
+import { Person, Product } from "./dataTypes.js";
+let people = [
+    new Person("Bob Smith", "London"),
+    new Person("Dora Peters", "New York"),
+];
+let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
+class PeopleCollection {
+    items = [];
+    constructor(intialItems) {
+        this.items.push(...intialItems);
+    }
+    add(newItem) {
+        this.items.push(newItem);
+    }
+    getNames() {
+        return this.items.map((item) => item.name);
+    }
+    getItem(index) {
+        return this.items[index];
     }
 }
-class ProductGroup {
-    constructor(...initialProducts) {
-        initialProducts.forEach(p => this[p[0]] = p[1]);
-    }
-}
-let group = new ProductGroup(["shoes", new SportsProduct("Shoes", "Running", 90.50)]);
-group.hat = new SportsProduct("Hat", "Skiing", 20);
-let total = group.hat.price + (group.boots?.price ?? 0);
-console.log(`Total: ${total}`);
+let peopleData = new PeopleCollection(people);
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
--- Chapter 12/Beginning of Chapter/types/src/index.ts
+++ Chapter 12/Beginning of Chapter/types/src/index.ts
@@ -1,8 +1,33 @@
 import { Person, Product } from "./dataTypes.js";
 
-let people = [new Person("Bob Smith", "London"), 
-    new Person("Dora Peters", "New York")];
+let people = [
+  new Person("Bob Smith", "London"),
+  new Person("Dora Peters", "New York"),
+];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 
-[...people, ...products].forEach(item => 
-    console.log(`Item: ${item.name}`));
+class PeopleCollection {
+  private items: Person[] = [];
+
+  constructor(intialItems: Person[]) {
+    this.items.push(...intialItems);
+  }
+
+  add(newItem: Person) {
+    this.items.push(newItem);
+  }
+
+  getNames(): string[] {
+    return this.items.map((item) => item.name);
+  }
+
+  getItem(index: number): Person {
+    return this.items[index];
+  }
+}
+
+let peopleData = new PeopleCollection(people);
+
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
