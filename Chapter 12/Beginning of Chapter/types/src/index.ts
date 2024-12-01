3a5fde37f9c644650699534a89e22e7b34b74096
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-01 21:49:52 +0100

    12.4 Defining generic interfaces

--- Chapter 12/Beginning of Chapter/types/src/index.ts
+++ Chapter 12/Beginning of Chapter/types/src/index.ts
@@ -1,43 +1,9 @@
 import { Person, Product, City, Employee } from "./dataTypes.js";
 
-let people = [
-  new Person("Bob Smith", "London"),
-  new Person("Dora Peters", "New York"),
-];
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-let cities = [new City("London", 8136000), new City("Paris", 2141000)];
-let employees = [
-  new Employee("Bob Smith", "Sales"),
-  new Employee("Alice Jones", "Sales"),
-];
+type shapeType = { name: string };
 
-class DataCollection<T> {
-  protected items: T[] = [];
-
-  constructor(intialItems: T[]) {
-    this.items.push(...intialItems);
-  }
-
-  // src/index.ts(22,56): error TS2693: 'V' only refers to a type, but is being used as a value here.
-  // filter<V extends T>(): V[] {
-  //    return this.items.filter((item) => item instanceof V) as V[];
-  // }
-
-  filter<V extends T>(predicate: (target) => target is V): V[] {
-    return this.items.filter((item) => predicate(item)) as V[];
-  }
-
-  static reverse<ArrayType>(items: ArrayType[]): ArrayType[] {
-    return items.reverse();
-  }
+interface Collection<T extends shapeType> {
+  add(...newItems: T[]): void;
+  get(name: string): T;
+  count: number;
 }
-
-let mixedData = new DataCollection<Person | Product>([...people, ...products]);
-function isProduct(target): target is Product {
-  return target instanceof Product;
-}
-let filteredProducts = mixedData.filter<Product>(isProduct);
-filteredProducts.forEach((p) => console.log(`Product: ${p.name}, ${p.price}`));
-
-let reversedCities = DataCollection.reverse<City>(cities);
-reversedCities.forEach((c) => console.log(`City: ${c.name}, ${c.population}`));
