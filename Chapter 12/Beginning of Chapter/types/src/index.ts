46cb1f17a967b997650b45475da147bd12457167
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-01 21:53:38 +0100

    12.4.1 Extending generic interfaces

--- Chapter 12/Beginning of Chapter/types/src/index.ts
+++ Chapter 12/Beginning of Chapter/types/src/index.ts
@@ -7,3 +7,15 @@ interface Collection<T extends shapeType> {
   get(name: string): T;
   count: number;
 }
+
+interface SearchableCollection<T extends shapeType> extends Collection<T> {
+  find(name: string): T | undefined;
+}
+
+interface ProductCollection extends Collection<Product> {
+  sumPrices(): number;
+}
+
+interface PeopleCollction<T extends Product | Employee> extends Collection<T> {
+  getNames(): string[];
+}
