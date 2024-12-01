fa361788ec43568b2f5c9c20ba486827b24df50c
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-01 22:25:17 +0100

    12.4.2 Implementing a generic interface - creating an abstract interface implementation

--- Chapter 12/Beginning of Chapter/types/dist/index.js
+++ Chapter 12/Beginning of Chapter/types/dist/index.js
@@ -1,16 +1,25 @@
-import { Person } from "./dataTypes.js";
-class PersonCollection {
+import { Person, Product } from "./dataTypes.js";
+class ArrayCollection {
     items = [];
     add(...newItems) {
         this.items.push(...newItems);
-    }
-    get(name) {
-        return this.items.find((item) => item.name === name);
     }
     get count() {
         return this.items.length;
     }
 }
+class ProductCollection extends ArrayCollection {
+    get(searchTerm) {
+        return this.items.find((item) => item.name === searchTerm);
+    }
+}
+class PersonCollection extends ArrayCollection {
+    get(searchTerm) {
+        return this.items.find((item) => item.name === searchTerm || item.city === searchTerm);
+    }
+}
 let peopleCollection = new PersonCollection();
 peopleCollection.add(new Person("Bob Smith", "London"), new Person("Dora Peters", "New York"));
-console.log(`Collection size: ${peopleCollection.count}`);
+let productCollection = new ProductCollection();
+productCollection.add(new Product("Running Shoes", 100), new Product("Hat", 25));
+[peopleCollection, productCollection].forEach((c) => console.log(`Size: ${c.count}`));
--- Chapter 12/Beginning of Chapter/types/src/index.ts
+++ Chapter 12/Beginning of Chapter/types/src/index.ts
@@ -8,19 +8,31 @@ interface Collection<T extends shapeType> {
   count: number;
 }
 
-class PersonCollection implements Collection<Person> {
-  private items: Person[] = [];
+abstract class ArrayCollection<T extends shapeType> implements Collection<T> {
+  protected items: T[] = [];
 
-  add(...newItems: Person[]): void {
+  add(...newItems: T[]): void {
     this.items.push(...newItems);
   }
 
-  get(name: string): Person {
-    return this.items.find((item) => item.name === name);
-  }
+  abstract get(name: string): T;
 
   get count(): number {
     return this.items.length;
+  }
+}
+
+class ProductCollection extends ArrayCollection<Product> {
+  get(searchTerm: string): Product {
+    return this.items.find((item) => item.name === searchTerm);
+  }
+}
+
+class PersonCollection extends ArrayCollection<Person> {
+  get(searchTerm: string): Person {
+    return this.items.find(
+      (item) => item.name === searchTerm || item.city === searchTerm
+    );
   }
 }
 
@@ -29,4 +41,13 @@ peopleCollection.add(
   new Person("Bob Smith", "London"),
   new Person("Dora Peters", "New York")
 );
-console.log(`Collection size: ${peopleCollection.count}`);
+
+let productCollection: Collection<Product> = new ProductCollection();
+productCollection.add(
+  new Product("Running Shoes", 100),
+  new Product("Hat", 25)
+);
+
+[peopleCollection, productCollection].forEach((c) =>
+  console.log(`Size: ${c.count}`)
+);
