ab640613053ab0aca7f563114832511cc76a8f27
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-01 22:13:21 +0100

    12.4.2 Implementing a generic interface - passing on the generic type parameter

--- Chapter 12/Beginning of Chapter/types/dist/index.js
+++ Chapter 12/Beginning of Chapter/types/dist/index.js
@@ -1,1 +1,16 @@
-export {};
+import { Person } from "./dataTypes.js";
+class ArrayCollection {
+    items = [];
+    add(...newItems) {
+        this.items.push(...newItems);
+    }
+    get(name) {
+        return this.items.find((item) => item.name === name);
+    }
+    get count() {
+        return this.items.length;
+    }
+}
+let peopleCollection = new ArrayCollection();
+peopleCollection.add(new Person("Bob Smith", "London"), new Person("Dora Peters", "New York"));
+console.log(`Collection size: ${peopleCollection.count}`);
--- Chapter 12/Beginning of Chapter/types/src/index.ts
+++ Chapter 12/Beginning of Chapter/types/src/index.ts
@@ -8,14 +8,27 @@ interface Collection<T extends shapeType> {
   count: number;
 }
 
-interface SearchableCollection<T extends shapeType> extends Collection<T> {
-  find(name: string): T | undefined;
+class ArrayCollection<DataType extends shapeType>
+  implements Collection<DataType>
+{
+  private items: DataType[] = [];
+
+  add(...newItems): void {
+    this.items.push(...newItems);
+  }
+
+  get(name: string): DataType {
+    return this.items.find((item) => item.name === name);
+  }
+
+  get count(): number {
+    return this.items.length;
+  }
 }
 
-interface ProductCollection extends Collection<Product> {
-  sumPrices(): number;
-}
-
-interface PeopleCollction<T extends Product | Employee> extends Collection<T> {
-  getNames(): string[];
-}
+let peopleCollection: Collection<Person> = new ArrayCollection<Person>();
+peopleCollection.add(
+  new Person("Bob Smith", "London"),
+  new Person("Dora Peters", "New York")
+);
+console.log(`Collection size: ${peopleCollection.count}`);
