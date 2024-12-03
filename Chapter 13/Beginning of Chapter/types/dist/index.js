78fcd352ec6d8df88264168e14a77f39cf15da0f
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 20:06:01 +0100

    13.5.5 Combining transformations in a single mapping

--- Chapter 13/Beginning of Chapter/types/dist/index.js
+++ Chapter 13/Beginning of Chapter/types/dist/index.js
@@ -1,7 +1,8 @@
 let p1 = { name: "Kayak" };
-let p2 = { name: "Kayak" };
-let p3 = { name: "Kayak" };
+let p2 = {
+    name: "Lifejacket",
+    price: 48.95,
+};
 console.log(`Custom mapped type: ${p1.name}`);
-console.log(`Built-in mapped type (Pick): ${p2.name}`);
-console.log(`Built-in mapped type (Omit): ${p3.name}`);
+console.log(`Built-in mapped type: ${p2.name}, ${p2.price}`);
 export {};
--- Chapter 13/Beginning of Chapter/types/src/index.ts
+++ Chapter 13/Beginning of Chapter/types/src/index.ts
@@ -1,12 +1,15 @@
 import { Product } from "./dataTypes.js";
 
-type SelectProperties<T, K extends keyof T> = {
-  [P in K]: T[P];
+type CustomMapped<T, K extends keyof T> = {
+  readonly [P in K]?: T[P];
 };
 
-let p1: SelectProperties<Product, "name"> = { name: "Kayak" };
-let p2: Pick<Product, "name"> = { name: "Kayak" };
-let p3: Omit<Product, "price"> = { name: "Kayak" };
+type BuiltInMapped<T, K extends keyof T> = Readonly<Partial<Pick<T, K>>>;
+
+let p1: CustomMapped<Product, "name"> = { name: "Kayak" };
+let p2: BuiltInMapped<Product, "name" | "price"> = {
+  name: "Lifejacket",
+  price: 48.95,
+};
 console.log(`Custom mapped type: ${p1.name}`);
-console.log(`Built-in mapped type (Pick): ${p2.name}`);
-console.log(`Built-in mapped type (Omit): ${p3.name}`);
+console.log(`Built-in mapped type: ${p2.name}, ${p2.price}`);
