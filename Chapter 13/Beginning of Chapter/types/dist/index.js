8e567dce770b84e821192064f7efd159103b7593
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 21:55:33 +0100

    13.6.6 Inferring additional types in conditions - using infer keyword

--- Chapter 13/Beginning of Chapter/types/dist/index.js
+++ Chapter 13/Beginning of Chapter/types/dist/index.js
@@ -8,6 +8,5 @@ function getValue(data, propName) {
     }
 }
 let products = [new Product("Kayak", 275), new Product("Lifejacket", 48.95)];
-// src/index.ts(12,48): error TS2345: Argument of type '"price"' is not assignable to parameter of type 'keyof Product[]'.
-// console.log(`Array Value: ${getValue(products, "price")}`);
+console.log(`Array Value: ${getValue(products, "price")}`);
 console.log(`Single Total: ${getValue(products[0], "price")}`);
--- Chapter 13/Beginning of Chapter/types/src/index.ts
+++ Chapter 13/Beginning of Chapter/types/src/index.ts
@@ -1,6 +1,8 @@
 import { Product } from "./dataTypes.js";
 
-function getValue<T, P extends keyof T>(data: T, propName: P): T[P] {
+type targetKeys<T> = T extends (infer U)[] ? keyof U : keyof T;
+
+function getValue<T, P extends targetKeys<T>>(data: T, propName: P): T[P] {
   if (Array.isArray(data)) {
     return data[0][propName];
   } else {
@@ -9,6 +11,5 @@ function getValue<T, P extends keyof T>(data: T, propName: P): T[P] {
 }
 
 let products = [new Product("Kayak", 275), new Product("Lifejacket", 48.95)];
-// src/index.ts(12,48): error TS2345: Argument of type '"price"' is not assignable to parameter of type 'keyof Product[]'.
-// console.log(`Array Value: ${getValue(products, "price")}`);
+console.log(`Array Value: ${getValue(products, "price")}`);
 console.log(`Single Total: ${getValue(products[0], "price")}`);
