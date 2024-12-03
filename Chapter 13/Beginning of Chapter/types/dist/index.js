e9a4fb62dee764ebf5194de6216614f8c6e0244f
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 18:09:48 +0100

    13.5.2 Using a generic type parameter with a mapped type

--- Chapter 13/Beginning of Chapter/types/dist/index.js
+++ Chapter 13/Beginning of Chapter/types/dist/index.js
@@ -1,7 +1,5 @@
 let p = { name: "Kayak", price: 275 };
 console.log(`Mapped type: ${p.name}, ${p.price}`);
-let q = { name: "Kayak", price: "apples" };
-console.log(`Changed type #1: ${q.name}, ${q.price}`);
-let r = { nameProperty: "Kayak", priceProperty: 12 };
-console.log(`Changed type #2: ${r.nameProperty}, ${r.priceProperty}`);
+let c = { name: "London", population: 8136000 };
+console.log(`Mapped type: ${c.name}, ${c.population}`);
 export {};
--- Chapter 13/Beginning of Chapter/types/src/index.ts
+++ Chapter 13/Beginning of Chapter/types/src/index.ts
@@ -1,20 +1,11 @@
-import { Product } from "./dataTypes.js";
+import { City, Product } from "./dataTypes.js";
 
-type MappedProduct = {
-  [P in keyof Product]: Product[P];
+type Mapped<T> = {
+  [P in keyof T]: T[P];
 };
 
-let p: MappedProduct = { name: "Kayak", price: 275 };
+let p: Mapped<Product> = { name: "Kayak", price: 275 };
 console.log(`Mapped type: ${p.name}, ${p.price}`);
 
-type AllowStrings = {
-  [P in keyof Product]: Product[P] | string;
-};
-let q: AllowStrings = { name: "Kayak", price: "apples" };
-console.log(`Changed type #1: ${q.name}, ${q.price}`);
-
-type ChangeNames = {
-  [P in keyof Product as `${P}Property`]: Product[P];
-};
-let r: ChangeNames = { nameProperty: "Kayak", priceProperty: 12 };
-console.log(`Changed type #2: ${r.nameProperty}, ${r.priceProperty}`);
+let c: Mapped<City> = { name: "London", population: 8136000 };
+console.log(`Mapped type: ${c.name}, ${c.population}`);
