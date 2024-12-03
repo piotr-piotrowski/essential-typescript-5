fe5ab9bc95389589a7fcc69b671262409ac1e1d3
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 22:06:06 +0100

    13.6.6 Inferring additional types in conditions - inferring types of functions using built-in conditional types

--- Chapter 13/Beginning of Chapter/types/dist/index.js
+++ Chapter 13/Beginning of Chapter/types/dist/index.js
@@ -1,8 +1,7 @@
-import { Product } from "./dataTypes.js";
-function processArray(data, func) {
-    return data.map((item) => func(item));
+import { City, Product } from "./dataTypes.js";
+function makeObject(constructor, ...args) {
+    return new constructor(...args);
 }
-let selectName = (p) => p.name;
-let products = [new Product("Kayak", 275), new Product("Lifejacket", 48.95)];
-let names = processArray(products, selectName);
-names.forEach((name) => console.log(`Name: ${name}`));
+let prod = makeObject(Product, "Kayak", 275);
+let city = makeObject(City, "London", 8136000);
+[prod, city].forEach((item) => console.log(`Name: ${item.name}`));
--- Chapter 13/Beginning of Chapter/types/src/index.ts
+++ Chapter 13/Beginning of Chapter/types/src/index.ts
@@ -1,16 +1,13 @@
-import { Product } from "./dataTypes.js";
+import { City, Product } from "./dataTypes.js";
 
-type Result<T> = T extends (...args: any) => infer R ? R : never;
-
-function processArray<T, Func extends (T) => any>(
-  data: T[],
-  func: Func
-): Result<Func>[] {
-  return data.map((item) => func(item));
+function makeObject<T extends new (...args: any) => any>(
+  constructor: T,
+  ...args: ConstructorParameters<T>
+): InstanceType<T> {
+  return new constructor(...(args as any[]));
 }
 
-let selectName = (p: Product) => p.name;
+let prod: Product = makeObject(Product, "Kayak", 275);
+let city: City = makeObject(City, "London", 8136000);
 
-let products = [new Product("Kayak", 275), new Product("Lifejacket", 48.95)];
-let names: string[] = processArray(products, selectName);
-names.forEach((name) => console.log(`Name: ${name}`));
+[prod, city].forEach((item) => console.log(`Name: ${item.name}`));
