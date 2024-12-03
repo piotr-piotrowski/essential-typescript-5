3b772a3d2021e0b498a4fde0e2306b0d4f5da779
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 18:05:57 +0100

    13.5.1 Changing mapping names and types

--- Chapter 13/Beginning of Chapter/types/dist/index.js
+++ Chapter 13/Beginning of Chapter/types/dist/index.js
@@ -1,3 +1,7 @@
 let p = { name: "Kayak", price: 275 };
 console.log(`Mapped type: ${p.name}, ${p.price}`);
+let q = { name: "Kayak", price: "apples" };
+console.log(`Changed type #1: ${q.name}, ${q.price}`);
+let r = { nameProperty: "Kayak", priceProperty: 12 };
+console.log(`Changed type #2: ${r.nameProperty}, ${r.priceProperty}`);
 export {};
--- Chapter 13/Beginning of Chapter/types/src/index.ts
+++ Chapter 13/Beginning of Chapter/types/src/index.ts
@@ -6,3 +6,15 @@ type MappedProduct = {
 
 let p: MappedProduct = { name: "Kayak", price: 275 };
 console.log(`Mapped type: ${p.name}, ${p.price}`);
+
+type AllowStrings = {
+  [P in keyof Product]: Product[P] | string;
+};
+let q: AllowStrings = { name: "Kayak", price: "apples" };
+console.log(`Changed type #1: ${q.name}, ${q.price}`);
+
+type ChangeNames = {
+  [P in keyof Product as `${P}Property`]: Product[P];
+};
+let r: ChangeNames = { nameProperty: "Kayak", priceProperty: 12 };
+console.log(`Changed type #2: ${r.nameProperty}, ${r.priceProperty}`);
