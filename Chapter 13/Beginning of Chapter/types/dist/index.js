7e06e3a36ddc115fc70b9989a1f55d35b65ca69f
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 18:01:16 +0100

    13.5 Using type mapping

--- Chapter 13/Beginning of Chapter/types/dist/index.js
+++ Chapter 13/Beginning of Chapter/types/dist/index.js
@@ -1,27 +1,3 @@
-import { Product } from "./dataTypes.js";
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-class Collection {
-    propertyName;
-    items;
-    constructor(initialItems = [], propertyName) {
-        this.propertyName = propertyName;
-        this.items = new Map();
-        this.add(...initialItems);
-    }
-    add(...newItems) {
-        newItems.forEach((newItem) => this.items.set(newItem[this.propertyName], newItem));
-    }
-    get(key) {
-        return this.items.get(key);
-    }
-    get count() {
-        return this.items.size;
-    }
-    [Symbol.iterator]() {
-        return this.items.values();
-    }
-}
-let productCollection = new Collection(products, "price");
-console.log(`There are ${productCollection.count} products`);
-let itemByKey = productCollection.get(100);
-console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
+let p = { name: "Kayak", price: 275 };
+console.log(`Mapped type: ${p.name}, ${p.price}`);
+export {};
--- Chapter 13/Beginning of Chapter/types/src/index.ts
+++ Chapter 13/Beginning of Chapter/types/src/index.ts
@@ -1,36 +1,8 @@
 import { Product } from "./dataTypes.js";
 
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
+type MappedProduct = {
+  [P in keyof Product]: Product[P];
+};
 
-class Collection<T, K extends keyof T> implements Iterable<T> {
-  private items: Map<T[K], T>;
-
-  constructor(initialItems: T[] = [], private propertyName: K) {
-    this.items = new Map<T[K], T>();
-    this.add(...initialItems);
-  }
-
-  add(...newItems: T[]): void {
-    newItems.forEach((newItem) =>
-      this.items.set(newItem[this.propertyName], newItem)
-    );
-  }
-
-  get(key: T[K]): T {
-    return this.items.get(key);
-  }
-
-  get count(): number {
-    return this.items.size;
-  }
-
-  [Symbol.iterator](): Iterator<T> {
-    return this.items.values();
-  }
-}
-
-let productCollection = new Collection(products, "price");
-console.log(`There are ${productCollection.count} products`);
-
-let itemByKey = productCollection.get(100);
-console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
+let p: MappedProduct = { name: "Kayak", price: 275 };
+console.log(`Mapped type: ${p.name}, ${p.price}`);
