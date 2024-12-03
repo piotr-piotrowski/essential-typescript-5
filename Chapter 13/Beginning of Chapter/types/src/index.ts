9af5aeae8447d27e2c537784a663dcc58872c38f
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-03 19:56:33 +0100

    13.5.4 Using the basic built-in mappings

--- Chapter 13/Beginning of Chapter/types/src/index.ts
+++ Chapter 13/Beginning of Chapter/types/src/index.ts
@@ -1,24 +1,24 @@
 import { Product } from "./dataTypes.js";
 
-type MakeOptional<T> = {
-  [P in keyof T]?: T[P];
-};
+// type MakeOptional<T> = {
+//   [P in keyof T]?: T[P];
+// };
 
-type MakeRequired<T> = {
-  [P in keyof T]-?: T[P];
-};
+// type MakeRequired<T> = {
+//   [P in keyof T]-?: T[P];
+// };
 
-type MakeReadOnly<T> = {
-  readonly [P in keyof T]: T[P];
-};
+// type MakeReadOnly<T> = {
+//   readonly [P in keyof T]: T[P];
+// };
 
 type MakeReadWrite<T> = {
   -readonly [P in keyof T]: T[P];
 };
 
-type optionalType = MakeOptional<Product>;
-type requiredType = MakeRequired<optionalType>;
-type readOnlyType = MakeReadOnly<requiredType>;
+type optionalType = Partial<Product>;
+type requiredType = Required<optionalType>;
+type readOnlyType = Readonly<requiredType>;
 type readWriteType = MakeReadWrite<readOnlyType>;
 
 let p: readWriteType = { name: "Kayak", price: 275 };
