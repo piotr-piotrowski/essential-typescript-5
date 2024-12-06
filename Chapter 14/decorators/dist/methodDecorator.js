8dc5bd99fae3b31e2e1b6cc45aef6173d6faeb50
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-06 15:24:24 +0100

    14.2.1 Using decorator context data

--- Chapter 14/decorators/dist/methodDecorator.js
+++ Chapter 14/decorators/dist/methodDecorator.js
@@ -1,5 +1,11 @@
-export function time(...args) {
-    return function () {
-        return "Hello, Decorator!";
+export function time(method, ctx) {
+    const methodName = String(ctx.name);
+    return function (...args) {
+        const start = performance.now();
+        console.log(`${methodName} started`);
+        const result = method.call(this, ...args);
+        const duration = (performance.now() - start).toFixed(2);
+        console.log(`${methodName} ended ${duration} ms`);
+        return result;
     };
 }
--- Chapter 14/decorators/src/methodDecorator.ts
+++ Chapter 14/decorators/src/methodDecorator.ts
@@ -1,5 +1,11 @@
-export function time(...args) {
-  return function (): string {
-    return "Hello, Decorator!";
+export function time(method: any, ctx: ClassMethodDecoratorContext) {
+  const methodName = String(ctx.name);
+  return function (this: any, ...args: any[]) {
+    const start = performance.now();
+    console.log(`${methodName} started`);
+    const result = method.call(this, ...args);
+    const duration = (performance.now() - start).toFixed(2);
+    console.log(`${methodName} ended ${duration} ms`);
+    return result;
   };
 }
