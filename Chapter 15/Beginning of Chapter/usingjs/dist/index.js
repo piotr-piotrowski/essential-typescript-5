49550ef9449040ad5eb240376c2d7298a02e71a9
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-13 18:57:28 +0100

    15.3.5 Using packages that include type declarations

--- Chapter 15/Beginning of Chapter/usingjs/dist/index.js
+++ Chapter 15/Beginning of Chapter/usingjs/dist/index.js
@@ -2,6 +2,7 @@ import { Cart } from "./cart.js";
 import { SPORT, SportsProduct } from "./product.js";
 import { sizeFormatter, costFormatter } from "./formatters.js";
 import debug from "debug";
+import chalk from "chalk";
 let kayak = new SportsProduct(1, "Kayak", 275, SPORT.Watersports);
 let hat = new SportsProduct(2, "Hat", 22.1, SPORT.Running);
 let ball = new SportsProduct(3, "Soccer Ball", 19.5, SPORT.Running);
@@ -14,3 +15,7 @@ costFormatter("Cart", `${cart.totalPrice}`);
 let db = debug("Example App");
 db.enabled = true;
 db("Message: %s", "Test message");
+console.log(chalk.greenBright("Formatted message"));
+// node_modules/chalk/source/vendor/supports-color/index.d.ts(1,32): error TS2307: Cannot find module 'node:tty' or its corresponding type declarations.
+// src/index.ts(24,19): error TS2339: Property 'notAColor' does not exist on type 'ChalkInstance'.
+// console.log(chalk.notAColor("Formatted message"));
