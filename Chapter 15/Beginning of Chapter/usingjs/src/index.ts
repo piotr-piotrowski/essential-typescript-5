9808b6117ad6640aca42d514106d7677a34152a7
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-13 18:18:17 +0100

    15.1.1 Adding TypeScript code to the example project

---
+++ Chapter 15/Beginning of Chapter/usingjs/src/index.ts
@@ -0,0 +1,14 @@
+import { Cart } from "./cart.js";
+import { SPORT, SportsProduct } from "./product.js";
+
+let kayak = new SportsProduct(1, "Kayak", 275, SPORT.Watersports);
+let hat = new SportsProduct(2, "Hat", 22.1, SPORT.Running);
+let ball = new SportsProduct(3, "Soccer Ball", 19.5, SPORT.Running);
+
+let cart = new Cart("Bot");
+cart.addProduct(kayak, 1);
+cart.addProduct(hat, 1);
+cart.addProduct(hat, 2);
+
+console.log(`Cart has ${cart.itemCount} items`);
+console.log(`Cart value is $${cart.totalPrice.toFixed(2)}`);
