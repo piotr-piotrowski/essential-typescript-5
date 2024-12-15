a4f91bfdfbf597c39cd31af3c5264e1362898c09
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-15 20:22:35 +0100

    16.6.1 Adding support for Bootstrap CSS styles

--- Chapter 16/webapp/webpack.config.js
+++ Chapter 16/webapp/webpack.config.js
@@ -3,9 +3,12 @@ module.exports = {
   devtool: "inline-source-map",
   entry: "./src/index.ts",
   output: { filename: "bundle.js" },
-  resolve: { extensions: [".ts", ".js"] },
+  resolve: { extensions: [".ts", ".js", ".css"] },
   module: {
-    rules: [{ test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }],
+    rules: [
+      { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
   },
   devServer: {
     static: "./assets",
