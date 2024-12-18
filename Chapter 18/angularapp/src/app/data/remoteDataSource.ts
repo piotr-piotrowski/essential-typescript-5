a8ecf22a8cdf006c579fcb078458fee1d2b621fc
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-18 21:11:16 +0100

    19.4 Containerizing the application

--- Chapter 18/angularapp/src/app/data/remoteDataSource.ts
+++ Chapter 18/angularapp/src/app/data/remoteDataSource.ts
@@ -5,13 +5,15 @@ import { map } from 'rxjs/operators';
 import { DataSourceImpl } from './dataSource';
 import { Product, Order } from './entities';
 
-const protocol = 'http';
-const hostname = 'localhost';
-const port = 4600;
+// const protocol = 'http';
+// const hostname = 'localhost';
+// const port = 4600;
 
 const urls = {
-  products: `${protocol}://${hostname}:${port}/products`,
-  orders: `${protocol}://${hostname}:${port}/orders`,
+  // products: `${protocol}://${hostname}:${port}/products`,
+  // orders: `${protocol}://${hostname}:${port}/orders`,
+  products: '/api/products',
+  orders: '/api/orders',
 };
 
 @Injectable()
